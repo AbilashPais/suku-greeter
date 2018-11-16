fd6316247b8abec5e35ff8811d8ace36f142caac
Lucas Henning <lhenning@citizensreserve.com>
2018-11-16 13:20:44 +0530

    added 10x

--- hello.js
+++ hello.js
@@ -1,5 +1,8 @@
 // This is the suku greeter
 
-console.log("Suku is great!");
+for (let i = 0; i <10; i++) {
+    console.log("Suku is great! " + i);
+}
+
 console.log("This is the development branch");
 
